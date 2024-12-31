470a8d80e6681042e243c969c3068b48bb4abb54
James Guleno <jguleno27@siprep.org>
2024-12-31 15:25:38 -0800

    improve collision and scoring

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    private Projectile projectile;
     [SerializeField] private Team team;
+    [SerializeField] private int score = 1;
 
     private void OnTriggerEnter(Collider other)
     {
-        projectile = other.GetComponent<Projectile>();
+        Projectile projectile = other.GetComponent<Projectile>();
         if (!projectile) projectile = other.GetComponentInParent<Projectile>();
         if (!projectile) return;
         
         Debug.Log("Adding score!");
-        ScoreManager.Instance.AddScore(team, 1);
+        ScoreManager.Instance.AddScore(team, score);
     }
 }
